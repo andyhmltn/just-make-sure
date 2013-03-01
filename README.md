097112b9989beb1279fd3f0924e31e771466adcc
gelbreak <assaf.gelber@gmail.com>
2013-03-01 18:12:21 +0200

    Added all files

---
+++ README.md
@@ -0,0 +1,56 @@
+# justmakesure.js
+
+justmakesure is an EXTREMELY minimal javascript testing tool.
+I was working on a small-ish project and just wanted to make sure a few things were working as intended. I had no internet access to download a library so I wrote this tool.
+It is meant to be as easy and quick as possible to test really basic functionality for small projects.
+
+## Usage
+Add justmakesure.js and justmakesure.css to an html page along with the file you want to test and a test file (or write your tests in your html), open up the html file in your favorite browser and see the results.
+*For example: *
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <script type="text/javascript" src="justmakesure.js"></script>
+            <link rel="stylesheet" type="text/css" href="justmakesure.css">
+            <!--
+                include the script you want to test
+            -->
+        </head>
+        <body>
+            <script type="text/javascript">
+                var a = 3;
+                
+                MAKE_SURE.after_each = function() { a = 3 };
+                
+                // This will pass
+                makeSure("Multiplying 3 by 4 gives 12", function() {
+                    a *= 4;
+                    return a === 12;
+                });
+                
+                // This will fail
+                makeSure("Adding 17 to 3 gives 21", function() {
+                    a += 17;
+                    return a === 21;
+                });
+                
+                // This will throw an exception
+                makeSure("Calling plusOne on 3 gives 4", function() {
+                    a.plusOne();
+                    return a === 4;
+                });
+                
+                // Testing objects
+                makeSure("An array of numbers is sorted correctly", function() {
+                    var actual   = [2, 1, 4, 3].sort(),
+                        expected = [1, 2, 3, 4];
+                    return JSON.stringify(actual) === JSON.stringify(expected);
+                });
+            </script>
+        </body>
+    </html>
+
+---
+
+Please feel free to use and imporove this tool!
+Email me with ideas or fork and pull.
---
+++ justmakesure.js
@@ -0,0 +1,83 @@
+/*
+ *
+ * justmakesure 0.0
+ * SUPER minimal javascript testing tool.
+ * USAGE: Include a link in an html page to this file and the css file
+ *        and call makeSure with a description of what you are testing
+ *        and a method that tests it.
+ *        Tests which return true will be considered passing.
+ *
+ * justmakesure can be found at: www.github.com/agelber/just-make-sure
+ *
+ */
+
+var MAKE_SURE = {
+    
+    // Configurable methods to run before and after each test
+    before_each : null,
+    after_each  : null,
+    
+    _tests  : {},
+    _total  :  0,
+    _passed :  0
+}
+
+// Method for users to add test case
+var makeSure = function(description, test) {
+    MAKE_SURE._tests[description] = test;
+}
+
+// Runs all the test cases and displays results
+var runTests = function() {
+    var resultList = document.getElementById('makesure-results');
+    
+    for (var description in MAKE_SURE._tests) {
+        
+        if (typeof(MAKE_SURE.before_each) === 'function') { MAKE_SURE.before_each(); }
+        
+        var testResult = document.createElement('li');
+        testResult.classList.add('makesure-result');
+        testResult.textContent = description + ' -> ';
+        try {
+            var result = MAKE_SURE._tests[description]();
+            if (result) {
+                testResult.classList.add('makesure-passed');
+                testResult.textContent += 'PASSED!';
+                MAKE_SURE._passed++;
+            } else {
+                testResult.classList.add('makesure-failed');
+                testResult.textContent += 'FAILED!';
+            }
+        } catch(e) {
+            testResult.classList.add('makesure-exception');
+            testResult.textContent += e.toString();
+        }
+        resultList.appendChild(testResult);
+        MAKE_SURE._total++;
+        
+        if (typeof(MAKE_SURE.after_each) === 'function') { MAKE_SURE.after_each(); }
+    }
+    updateTotals();
+}
+
+// Updates '#makesure-totals' with the results of the tests
+var updateTotals = function() {
+    var totals = document.getElementById('makesure-totals');
+    totals.textContent = MAKE_SURE._passed + '/' + MAKE_SURE._total + ' tests passed';
+}
+
+// when the document has finished loading, creates divs in which the results will be displayed and runs the tests
+document.onreadystatechange = function() {
+    if (document.readyState === 'complete') {
+        var container = document.createElement('div'),
+            results   = document.createElement('ul'),
+            totals    = document.createElement('div');
+        container.setAttribute('id', 'makesure');
+        results.setAttribute('id', 'makesure-results');
+        totals.setAttribute('id', 'makesure-totals');
+        document.body.appendChild(container);
+        container.appendChild(results);
+        container.appendChild(totals);
+        runTests();
+    }
+}
